18259d6aeeb2092b7ddbf9e5a7382d6e9a8d0a53
SanguHK <sanghk143@gmail.com>
2026-05-06 14:36:13 +0530

    Set operator

---
+++ setoperator.sql
@@ -0,0 +1,19 @@
+SET:
+It is used to combine multiple queries output.
+EX:q1={1,2,3,4,5} 
+
+    q2={2,5,6}
+
+i)UNION
+It provides both common and uncommon values without duplicate values.
+
+Ex: q1 U q2
+={1,2,3,4,5,6}
+
+
+ii)UNION ALL
+It provides both common and uncommon values with duplicate values.
+
+Ex: q1 U q2
+={1,2,2,3,4,5,5,6}
+
